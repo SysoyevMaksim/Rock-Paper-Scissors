aef006151677d7e71d7ce42d7422c62479811260
Maksim <sysoyev.maxim@gmail.com>
2020-10-30 13:24:25 +0300

    Stage 2/5: Equalizing chances

--- task/rps/game.py
+++ task/rps/game.py
@@ -1,9 +1,16 @@
-users_input = input()
-wining_thing = ""
-if users_input == "paper":
-    wining_thing = "scissors"
-elif users_input == "scissors":
-    wining_thing = "rock"
-elif users_input == "rock":
-    wining_thing = "paper"
-print("Sorry, but the computer chose " + wining_thing)
+import random
+
+
+options = ('rock', 'paper', 'scissors')
+user_choice = input()
+computer_choice = random.choice(options)
+if user_choice == computer_choice:
+    print(f"There is a draw ({user_choice})")
+else:
+    user_choice_number = options.index(user_choice)
+    computer_choice_number = options.index(computer_choice)
+    user_wins = user_choice_number == (computer_choice_number + 1) % 3
+    if user_wins:
+        print(f"Well done. The computer chose {computer_choice} and failed")
+    else:
+        print(f"Sorry, but the computer chose {computer_choice}")
